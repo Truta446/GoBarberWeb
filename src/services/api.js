f8b32a9b085f622ed3829c7f7ed70bef64fdd908
Juan Versolato Lopes <juan.versolato@outlook.com>
2021-07-19 10:46:33 -0400

    Chore: Change HTTP to HTTPS.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://52.88.150.170' : 'http://localhost:3333',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://52.88.150.170' : 'http://localhost:3333',
 });
 
 export default api;
